7f901c8b813b664dcd55e52d5058d66f893c5349
xbwangh <xbwangh@isoftstone.com>
2017-12-25 18:01:32 +0800

    commit

--- numpy/example05.py
+++ numpy/example05.py
@@ -7,7 +7,14 @@ def f(x):
     return np.sin(x)
 
 
-x = np.arange(-10, 10, 0.1)
+def c(x):
+    return np.cos(x)
+
+
+x = np.arange(-9.5, 9.5, 0.1)
 plt.plot(x, f(x))
-plt.axis([-10, 10, -1, 1])
+plt.plot(x, c(x), color='y')
+plt.axhline(color='r')
+plt.axvline(color='r')
+plt.axis([-9.5, 9.5, -1, 1])
 plt.show()
