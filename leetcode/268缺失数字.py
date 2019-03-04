f19af0a8aba7f58fe642054e3d79877fbc02ae3f
technology@shellunion.com <anini456123>
2019-03-04 09:40:29 +0800

    268

---
+++ leetcode/268缺失数字.py
@@ -0,0 +1,26 @@
+# 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+# 示例 1:
+# 输入: [3,0,1]
+# 输出: 2
+# 示例 2:
+# 输入: [9,6,4,2,3,5,7,0,1]
+# 输出: 8
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, sum = len(nums), 0
+        for i in range(l+1):
+            sum ^= i
+            if i < l:
+                sum ^= nums[i]
+        return sum
+
+
+solution = Solution()
+result = solution.missingNumber([3,0,1,2,5])
+print(result)
