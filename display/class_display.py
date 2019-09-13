fe09f9d9d746f6fecc15fa761e96b8c5b15789be
bobowang2017 <409879654@qq.com>
2019-09-13 09:04:13 +0800

    commit

--- display/class_display.py
+++ display/class_display.py
@@ -28,21 +28,24 @@ class SpringBootBuildParam(BuildParam):
     DEPLOYMENT_URL = "who am i "
 
 
-def get_build_params(_type):
+def get_build_params(_type, **kwargs):
     build_params = None
     for _class in BuildParam.__subclasses__():
+        print(_class.__name__)
         if hasattr(_class, "build_key"):
             if getattr(_class, "build_key") == _type:
                 build_params = _class
                 break
     if build_params:
-        return build_params()
+        constant = build_params()
+        for _k, _v in kwargs.items():
+            constant.__dict__[_k.upper()] = _v
+        return constant
     else:
-        raise Exception("Not Found")
+        raise Exception("Not Found BuildParam {}".format(_type))
 
 
-build_constant = get_build_params("java")
-build_constant.BOBO = 'fwfe'
+build_constant = get_build_params("javac", pipeline_id="woshiwangxiangbo")
 params = {_property: getattr(build_constant, _property) for _property in dir(build_constant) if _property.isupper()}
 for _k, _v in params.items():
     print(_k, _v)
