0df14880985e303dceb92b15b8513ca26017a106
bobowang2017 <409879654@qq.com>
2020-05-09 15:23:52 +0800

    commit

---
+++ algorithm/leetcode/1/1010.总持续时间可被 60 整除的歌曲.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# 在歌曲列表中，第 i 首歌曲的持续时间为 time[i] 秒。
+#
+# 返回其总持续时间（以秒为单位）可被 60 整除的歌曲对的数量。形式上，我们希望索引的数字 i 和 j 满足  i < j 且有 (time[i] + time[j]) % 60 == 0。
+#
+#  
+#
+# 示例 1：
+#
+# 输入：[30,20,150,100,40]
+# 输出：3
+# 解释：这三对的总持续时间可被 60 整数：
+# (time[0] = 30, time[2] = 150): 总持续时间 180
+# (time[1] = 20, time[3] = 100): 总持续时间 120
+# (time[1] = 20, time[4] = 40): 总持续时间 60
+# 示例 2：
+#
+# 输入：[60,60,60]
+# 输出：3
+# 解释：所有三对的总持续时间都是 120，可以被 60 整数。
+#  
+#
+# 提示：
+#
+# 1 <= time.length <= 60000
+# 1 <= time[i] <= 500
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/pairs-of-songs-with-total-durations-divisible-by-60
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# 解题思路：该题有点类似两数之和，与之不同的是需要计算出所有的配对数。
+
+
+class Solution(object):
+    def numPairsDivisibleBy60(self, time):
+        """
+        :type time: List[int]
+        :rtype: int
+        """
+        temp = [0 for i in range(60)]
+        total = 0
+        for t in time:
+            _t = t % 60
+            if _t != 0:
+                total += temp[60 - _t]
+            else:
+                total += temp[_t]
+            temp[_t] += 1
+        return total
+
+
+s = Solution()
+res = s.numPairsDivisibleBy60([30, 20, 150, 100, 40])
+# res = s.numPairsDivisibleBy60([60, 60, 60])
+print(res)
