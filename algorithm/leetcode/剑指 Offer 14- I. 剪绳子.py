32425c08fbe9ce7a7f435d98387577eb627b8065
bobowang2017 <anini456123>
2020-07-07 17:53:45 +0800

    剪绳子

---
+++ algorithm/leetcode/剑指 Offer 14- I. 剪绳子.py
@@ -0,0 +1,48 @@
+# 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。
+# 请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+#
+# 示例 1：
+#
+# 输入: 2
+# 输出: 1
+# 解释: 2 = 1 + 1, 1 × 1 = 1
+# 示例 2:
+#
+# 输入: 10
+# 输出: 36
+# 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/jian-sheng-zi-lcof
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+import functools
+
+
+class Solution(object):
+
+    def cuttingRope(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        @functools.lru_cache()
+        def rec_compute(s):
+            if s == 2:
+                return 2
+            if s == 3:
+                return 3
+            res = -1
+            for _s in range(1, s):
+                result = rec_compute(s - _s) * _s
+                if result > res:
+                    res = result
+            return res
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        return rec_compute(n)
+
+
+s = Solution()
+print(s.cuttingRope(6))
