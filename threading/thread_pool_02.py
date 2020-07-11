36627ddd0300d44a873303b7ad0557704f55b4b6
bobowang2017 <409879654@qq.com>
2020-07-11 12:37:28 +0800

    一个简单的线程池

--- threading/thread_pool_02.py
+++ threading/thread_pool_02.py
@@ -5,19 +5,23 @@ from threading import Thread
 class ThreadPoolManager(object):
 
     def __init__(self, thread_num):
+        # 这里定义的是一个线程安全的队列
         self.work_queue = queue.Queue()
+        # 定义线程池最大线程数量
         self.thread_num = thread_num
+        # 初始化工作线程
+        self.__init_threading_pool()
 
     def __init_threading_pool(self):
         for i in range(self.thread_num):
-            thread = ThreadManager(self.work_queue)
+            thread = WorkThread(self.work_queue)
             thread.start()
 
     def add_jobs(self, func, *args):
         self.work_queue.put((func, args))
 
 
-class ThreadManager(Thread):
+class WorkThread(Thread):
 
     def __init__(self, work_queue):
         Thread.__init__(self)
@@ -25,7 +29,8 @@ class ThreadManager(Thread):
 
     def run(self):
         while True:
-            target, args = self.work_queue.get()
+            # 从队列里取元素，如果block=True,则一直阻塞到有可用元素为止。
+            target, args = self.work_queue.get(block=False)
             target(*args)
             self.work_queue.task_done()
 
