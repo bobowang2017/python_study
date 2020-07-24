c7a6c55a16718da87aee3897d22efa097151106c
bobowang2017 <anini456123>
2020-07-24 21:32:10 +0800

    rabbitmq

--- rabbitmq/helloworld/send.py
+++ rabbitmq/helloworld/send.py
@@ -7,7 +7,13 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
     credentials=pika.PlainCredentials("devops", "q9wCFiEti7UuxYFPr3q0Xw")
 ))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='hello', durable=True, passive=True)
+channel.queue_declare(queue='hello', durable=True, passive=True)
+channel.exchange_declare(exchange="task.rec.tt", durable=True, exchange_type="direct", passive=True)
+# 交换机和队列绑定:生产者发布消息时无需绑定，消费者消费消息时需要绑定
+channel.queue_bind(exchange="task.rec.tt", queue="hello", routing_key="world1")
+channel.queue_bind(exchange="task.rec.tt", queue="world", routing_key="world2")
+channel.basic_qos(prefetch_count=1)
 for i in range(10):
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    channel.basic_publish(exchange='task.rec.tt', routing_key='world1', body=f'Hello World--{i}')
 connection.close()
