341c2256530e0ba46884408b3db8bfa0be95761c
wangxiangbo <wangxiangbo@cmsr.chinamobile.com>
2021-06-28 20:21:16 +0800

    八皇后问题

--- algorithm/leetcode/面试题 08.12. 八皇后.py
+++ algorithm/leetcode/面试题 08.12. 八皇后.py
@@ -3,28 +3,28 @@ class Solution(object):
     def __init__(self):
         self.board = None
         self.length = -1
+        self.result = []
 
     def solveNQueens(self, n):
         """
         :type n: int
         :rtype: List[List[str]]
         """
-        self.board = [[-1 for _ in range(n)] for _ in range(n)]
+        self.board = [['.' for _ in range(n)] for _ in range(n)]
         self.length = n
         self.back_track(0)
+        return self.result
 
     def back_track(self, row):
         if row == self.length:
-            print(self.board)
+            self.result.append([''.join(self.board[x]) for x in range(self.length)])
             return
         for c in range(self.length):
-            print(row)
             if not self.is_valid(row, c):
                 continue
             self.board[row][c] = 'Q'
-
             self.back_track(row + 1)
-            self.board[row][c] = -1
+            self.board[row][c] = '.'
 
     def is_valid(self, row, col):
         # 检查列是否有皇后互相冲突
@@ -43,7 +43,7 @@ class Solution(object):
         # 检查左上方是否有皇后互相冲突
         _i = row - 1
         _j = col - 1
-        while _i >= 0 and _j < self.length:
+        while _i >= 0 and 0 <= _j:
             if self.board[_i][_j] == 'Q':
                 return False
             _i -= 1
